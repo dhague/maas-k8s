3189b7d2e82097cf86a103a8eb26ee049ea18d1a
Darren Hague <d.hague@sap.com>
2017-07-10 14:55:42 +0100

    Enable GPU support in kubelet

--- scripts/k8s-packages.sh
+++ scripts/k8s-packages.sh
@@ -1,4 +1,12 @@
 #/bin/bash
+
+# Create kubelet service override file to enable GPU support
+sudo mkdir /etc/systemd/system/kubelet.service.d
+cat <<EOF | sudo tee /etc/systemd/system/kubelet.service.d/override.conf > /dev/null
+[Service]
+Environment='KUBELET_EXTRA_ARGS=--feature-gates="Accelerators=true"'
+EOF
+
 # Install kubectl, kubelet and kubeadm
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
--- scripts/k8s-worker-gpu.sh
+++ scripts/k8s-worker-gpu.sh
@@ -10,17 +10,15 @@
 
 wget --directory-prefix=/tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
 sudo dpkg -i /tmp/nvidia-docker_1.0.1-1_amd64.deb
-#sudo ln -s .../volumes/nvidia_driver/375.31 /usr/local/lib/nvidia
+sudo ln -s /var/lib/nvidia-docker/volumes/nvidia_driver/* /usr/local/lib/nvidia
 
-# Install kubectl, kubelet and kubeadm
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-#cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
-#deb http://apt.kubernetes.io/ kubernetes-xenial main
-#EOF
-#sudo apt-get update
-#sudo apt-get install -y kubeadm kubectl
+NVIDIA_GPU_NAME=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader --id=0)
+cat <<EOF | sudo tee /etc/systemd/system/kubelet.service.d/override.conf > /dev/null
+[Service]
+Environment="KUBELET_EXTRA_ARGS=--feature-gates='Accelerators=true' --node-labels='alpha.kubernetes.io/nvidia-gpu-name=$NVIDIA_GPU_NAME'"
+EOF
 
 # Join K8S Master
-#sudo kubeadm reset # Workaround for https://github.com/kubernetes/kubeadm/issues/1
-#sudo kubeadm join --token $TOKEN $K8S_HOSTIP
+sudo kubeadm reset # Workaround for https://github.com/kubernetes/kubeadm/issues/1
+sudo kubeadm join --token $TOKEN $K8S_HOSTIP
 
