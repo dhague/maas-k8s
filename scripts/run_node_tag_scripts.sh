740533c72521c8122ecba80623f904d87d859aad
Darren Hague <d.hague@sap.com>
2017-07-11 12:29:22 +0100

    Extra carriage return needed for node tags

--- scripts/run_node_tag_scripts.sh
+++ scripts/run_node_tag_scripts.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 while read SCRIPT
 do
+    echo "Checking for script $SCRIPT.sh"
     if [ -e $SCRIPT.sh ]
     then
-        ./$SCRIPT.sh $@
+        echo "Running $SCRIPT.sh"
+        ./$SCRIPT.sh $@ && echo "$SCRIPT.sh succeeded" || echo "$SCRIPT.sh failed"
     fi
 done < ../../node_tags
 
