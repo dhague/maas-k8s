87f931d386d8b991bb94f4bc473133f7e2b087cf
Darren Hague <d.hague@sap.com>
2017-07-11 15:25:45 +0100

    Add ability to specify subnet for K8S API

--- scripts/k8s-master.sh
+++ scripts/k8s-master.sh
@@ -1,10 +1,16 @@
 #/bin/bash
+
 ./network.sh
 ./docker.sh
 ./k8s-packages.sh
 
 # Initialise K8S Master
-sudo kubeadm init
+# If an parameter is specified, use it to match the desired API server subnet address (e.g. '192.168.8.0')
+if [ '' != "$1" ] ; then
+  MASTER_IP=`ip addr show  | grep "inet\b" | awk '{print $2}' | cut -d/ -f1 | grep "${1/%.0/}"`
+  : ${MASTER_IP_ARG:="--apiserver-advertise-address=$MASTER_IP"}
+fi
+sudo kubeadm init $MASTER_IP_ARG
 
 # Set up kube config
 mkdir -p $HOME/.kube
