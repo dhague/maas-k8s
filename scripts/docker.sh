c38488b9b6efb06868537d34eb309f81f56f82be
Darren Hague <d.hague@sap.com>
2017-08-07 18:13:10 +0100

    Set proxy for docker if it's in /etc/environment

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,3 +5,5 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update 
 sudo apt-get install -y docker-ce
 
+# Copy proxy lines from /etc/environment to /etc/default/docker
+for line in $( grep proxy /etc/environment ) ; do echo 'export '$line | sudo tee -a /etc/default/docker > /dev/null ; done
