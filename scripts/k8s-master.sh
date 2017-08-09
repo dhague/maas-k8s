a385ca910248964780690fdd04d3bd0d224ad190
Darren Hague <d.hague@sap.com>
2017-08-09 17:50:15 +0100

    Make things work when using env vars as well as cmd line args

--- scripts/k8s-master.sh
+++ scripts/k8s-master.sh
@@ -2,12 +2,16 @@
 # If an parameter is specified, use it to match the desired API server subnet address (e.g. '192.168.8.0')
 if [ '' != "$1" ] ; then
   MASTER_IP=`ip addr show  | grep "inet\b" | awk '{print $2}' | cut -d/ -f1 | grep "${1/%.0/}"`
+fi
+if [ '' != "$MASTER_IP" ] ; then
   MASTER_IP_ARG="--apiserver-advertise-address=$MASTER_IP"
 fi
 if [ '' != "$2" ] ; then
   TOKEN=$2
   # Token must match regex [a-z0-9]{6}\.[a-z0-9]{16}
   [[ $TOKEN =~ ^[a-z0-9]{6}\.[a-z0-9]{16}$ ]] || (echo "`basename "$0"`: Token is invalid" && exit 1)
+fi
+if [ '' != "$TOKEN" ] ; then
   TOKEN_ARG="--token $TOKEN"
 fi
 
---
+++ scripts/proxy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+if [ '' != "$PROXY_URL" ] ; then
+    cat <<EOF | sudo tee /etc/apt.conf > /dev/null
+    Acquire::http::Proxy "$PROXY_URL";
+    EOF
+
+    cat <<EOF | sudo tee -a /etc/environment > /dev/null
+http_proxy=$PROXY_URL
+https_proxy=$PROXY_URL
+HTTP_PROXY=$PROXY_URL
+HTTPS_PROXY=$PROXY_URL
+EOF
+
+    for line in $( grep -e proxy -e PROXY /etc/environment ) ; do export $line ; done
+
+    git config --global http.proxy $PROXY_URL
+fi
