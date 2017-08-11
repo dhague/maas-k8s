6efb674fd7c27ede0da392fc242651d9c518a44e
Darren Hague <d.hague@sap.com>
2017-08-11 13:08:36 +0100

    Need to update apt cache before installing nfs-common

--- scripts/network.sh
+++ scripts/network.sh
@@ -5,5 +5,6 @@ if [ "$MAAS_GW" != "$INET_GW" ] ; then
 fi
 
 # Make sure we can mount NFS (host needs to be able to do this for Kubernetes to do it)
+sudo apt-get update
 sudo apt-get install -y nfs-common
 
