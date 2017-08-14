4b154130f3fd9ad2665685c5d5294c0f7940b3c8
Darren Hague <d.hague@sap.com>
2017-08-14 14:22:04 +0100

    Allow for the fscache directory to be set via env var

--- scripts/cachefilesd.sh
+++ scripts/cachefilesd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get install -y cachefilesd
 sudo sed -i -e 's/#RUN=yes/RUN=yes/g' /etc/default/cachefilesd
+: ${FSCACHE_DIR:=/var/cache/fscache}
 cat <<EOF | sudo tee /etc/cachefilesd.conf > /dev/null
-dir /var/cache/fscache
-#dir /raid
+dir $FSCACHE_DIR
 tag dgx1cache
 brun 25%
 bcull 15%
